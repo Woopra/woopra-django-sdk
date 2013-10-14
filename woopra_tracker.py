c289d429f1b3bf637cc0dc7ad3b0ffcd470b8c38
antoinechkaiban <antoine.chkaiban@student.ecp.fr>
2013-10-14 12:05:28 -0700

    WoopraTracker class now only implements back-end tracking

--- woopra_tracker.py
+++ woopra_tracker.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import string
-import random
-import json
 import urllib
 import httplib
+import hashlib
 
 class WoopraTracker:
 	"""
@@ -12,46 +10,81 @@ class WoopraTracker:
 	This class represents the Python equivalent of the JavaScript Woopra Object.
 	"""
 
-	# Default Configuration Dictionary
-	DEFAULT_CONFIG = {
-		"domain" : "", 
-		"cookie_name" : "wooTracker",
-		"cookie_domain" : "",
-		"cookie_path" : "/",
-		"ping" : True,
-		"ping_interval" : 12000,
-		"idle_timeout" : 300000,
-		"download_tracking" : True,
-		"outgoing_tracking" : True,
-		"download_pause" : 200,
-		"outgoing_pause" : 400,
-		"ignore_query_url" : True,
-		"hide_campaign" : False,
-		"ip_address" : "",
-		"cookie_value" : "",
-		"user_agent" : "",
-		"current_url" : ""
-	}
+	default_timeout = 300000
+	EMAIL = "email"
+	UNIQUE_ID = "unique_id"
 
-	def __init__(self):
+	def __init__(self, domain):
 		"""
 		The constructor.
 		Parameter:
-			None
+			domain - str : the domain name of your website as submitted on woopra.com
 		Result:
 			WoopraTracker
 		"""
+		self.domain = domain
+		self.idle_timeout = WoopraTracker.default_timeout
+		self.user_properties = {}
+		self.cookie_value = None
+		self.user_agent = None
+		self.ip_addresss = None
 
-		self.current_config = WoopraTracker.DEFAULT_CONFIG
-		self.custom_config = {}
+	def identify(self, identifier, value, user_properties = {}, ip_address = None, user_agent = None):
+		"""
+		Identifies a user.
+		Parameters:
+			identifier:
+				WoopraTracker.EMAIL to identify the user with his email (will generate unique ID automatically with a hash of the email)
+				WoopraTracker.UNIQUE_ID to identify the user with a unique ID directly
+			value - str : the value of the identifier (email or unique ID)
+			properties (optional) - dict : the user's additional properties (name, company, ...)
+				key - str : the user property name
+				value -str, int, bool = the user property value
+			ip_address (optional) - str : the IP address of the user.
+			user_agent (optional) - str : the value of the user_agent header
+		"""
+		if identifier == WoopraTracker.EMAIL:
+			self.user_agent = user_agent
+			self.ip_address = ip_address
+			self.user_properties = user_properties
+			self.user_properties["email"] = value
+			m = hashlib.md5()
+			m.update(value)
+			long_cookie = m.hexdigest().upper()
+			self.cookie_value = (long_cookie[:12]) if len(long_cookie) > 12 else long_cookie
+		elif identifier == WoopraTracker.UNIQUE_ID:
+			self.user_agent = user_agent
+			self.ip_address = ip_address
+			self.user_properties = user_properties
+			self.cookie_value = value
+		else:
+			print "Wrong identifier. Accepted values are WoopraTracker.EMAIL or WoopraTracker.UNIQUE_ID"
 
-		self.user = {}
-		self.events = []
+	def track(self, event_name, event_data = {}):
+		"""
+		Tracks pageviews and custom events
+		Parameters:
+			event_name (optional) - str : The name of the event. If none is specified, will track pageview
+			event_data (optional) - dict : Properties the custom event
+				key - str : the event property name
+				value - str, int, bool : the event property value
+		Examples:
+			# This code tracks a custom event through the back-end:
+			woopra.track('signup', {'company' : 'My Business', 'username' : 'johndoe', 'plan' : 'Gold'})
+		"""
+		self.woopra_http_request(True, event_name, event_data)
 
-		self.user_up_to_date = True
-		self.has_pushed = False
+	def push(self):
+		"""
+		Pushes the indentification information on the user to Woopra in case no tracking event occurs.
+		Parameter:
+			back_end_tracking (optional) - boolean : Should the information be pushed through the back-end? Defaults to False.
+		Result:
+			None
+		"""
+		self.woopra_http_request(False)
 
-	def woopra_http_request(self, is_tracking, event = None):
+	def woopra_http_request(self, is_tracking, event_name = None, event_data = {}):
 		"""
 		Sends an Http Request to Woopra for back-end identification and/or tracking.
 		Parameters:
@@ -64,148 +97,29 @@ class WoopraTracker:
 		get_params = {}
 
 		# Configuration
-		get_params["host"] = self.current_config["domain"]
-		get_params["cookie"] = self.current_config["cookie_value"]
-		get_params["ip"] = self.current_config["ip_address"]
-		get_params["timeout"] = self.current_config["idle_timeout"]
+		get_params["host"] = self.domain
+		get_params["cookie"] = self.cookie_value
+		if self.ip_address != None:
+			get_params["ip"] = self.ip_address
+		if self.idle_timeout != None:
+			get_params["timeout"] = self.idle_timeout
 
 		# Identification
-		for k, v in self.user.iteritems():
+		for k, v in self.user_properties.iteritems():
 			get_params["cv_" + k] = v
 
 		if not is_tracking:
 			url = "/track/identify/?" + urllib.urlencode(get_params)
 		else:
-			if event == None:
-				get_params["ce_name"] = "pv"
-				get_params["ce_url"] = self.current_config["current_url"]
-			else:
-				get_params["ce_name"] = event[0]
-				for k,v in event[1].iteritems():
-					get_params["ce_" + k] = v
+			get_params["ce_name"] = event_name
+			for k,v in event_data.iteritems():
+				get_params["ce_" + k] = v
 			url = "/track/ce/?" + urllib.urlencode(get_params)
-
-		user_agent = {'User-agent': self.request.META['HTTP_USER_AGENT']}
 		try:
 			conn = httplib.HTTPConnection(base_url)
-			conn.request("GET", url, headers=user_agent)
+			if self.user_agent != None:
+				conn.request("GET", url, headers={'User-agent': self.user_agent})
+			else:
+				conn.request("GET", url)
 		except HTTPException:
 			print "exception occured"
-
-	def js_code(self):
-		"""
-		This method returns the woopra code string to be passed to the Template, and outputed in its header.
-		Parameters:
-			None
-		Result:
-			code - str : The front-end tracking code.
-		"""
-		code = '\n   <!-- Woopra code starts here -->\n   <script>\n      (function(){\n      var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)\n      })("woopra");\n'
-		if len(self.custom_config) != 0:
-			code += "      woopra.config(" + json.dumps(self.custom_config) + ");\n"
-		if not self.user_up_to_date:
-			code += "      woopra.identify(" + json.dumps(self.user) + ");\n"
-		if len(self.events) != 0:
-			for event in self.events:
-				if event[0] == None:
-					code += "      woopra.track();\n"
-				else:
-					code += "      woopra.track('" + event[0] + "', " + json.dumps(event[1]) + ");\n"
-		if self.has_pushed:
-			code += "      woopra.push();\n"
-		code += "   </script>\n   <!-- Woopra code ends here -->\n"
-		return code
-
-	def config(self, data):
-		"""
-		Configure WoopraTracker Object.
-		Parameter:
-			data - dict :
-				key - str : the configuration option
-				value - str, int, bool : the configuration value
-		Result:
-			WoopraTracker
-		Example:
-			config({'domain' : 'mywebsite.com', 'ping' : True})
-		"""
-		for k, v in data.iteritems():
-			if k in WoopraTracker.DEFAULT_CONFIG:
-				self.current_config[k] = v
-				if k != "ip_address" and k != "cookie_value":
-					self.custom_config[k] = v
-					if k == "cookie_name":
-						self.actualize_cookie
-		return self
-
-	def identify(self, user):
-		"""
-		Identifies the user currently being tracked.
-		Parameters:
-			user - dict :
-				key - str : the user property name
-				value -str, int, bool = the user property value
-		Example:
-			identify({'email' : 'johndoe@mybusiness.com', 'name' : 'John Doe', 'company' : 'My Business'})
-		"""
-		self.user = user
-		self.user_up_to_date = False
-		return self
-
-	def track(self, event_name = None, event_data = None, back_end_tracking = False):
-		"""
-		Tracks pageviews and custom events
-		Parameters:
-			event_name (optional) - str : The name of the event. If none is specified, will track pageview
-			event_data (optional) - dict : Properties the custom event
-				key - str : the event property name
-				value - str, int, bool : the event property value
-			back_end_tracking (optional) - bool : Should this event be tracked through the back-end?
-		Examples:
-			# Track a pageview through the front-end:
-			track()
-			# Track a pageview through the back-end:
-			track(True)
-			# Track a custom event through the front-end:
-			woopra.track("play", {'artist' : 'Dave Brubeck', 'song' : 'Take Five', 'genre' : 'Jazz'})
-			# Track a custom event through the back-end:
-			woopra.track('signup', {'company' : 'My Business', 'username' : 'johndoe', 'plan' : 'Gold'}, True)
-		"""
-		if back_end_tracking:
-			self.woopra_http_request(True, [event_name, event_data])
-		else:
-			self.events += [[event_name, event_data]]
-		return self
-
-	def push(self, back_end_tracking = False):
-		"""
-		Pushes the indentification information on the user to Woopra in case no tracking event occurs.
-		Parameter:
-			back_end_tracking (optional) - boolean : Should the information be pushed through the back-end? Defaults to False.
-		Result:
-			None
-		"""
-		if not self.user_up_to_date:
-			if back_end_tracking:
-				self.woopra_http_request(False)
-			else:
-				self.has_pushed = True
-
-	def random_cookie(self):
-		"""
-		Generates a random 12 characters (Capital Letters and Numbers)
-		Parameter:
-			None
-		Result:
-			None
-		"""
-		return ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(12))
-
-	def actualize_cookie(self):
-		"""
-		If the cookie name changes, call this method to see if a cookie matches the new name, and update cookie_value.
-		Parameter:
-			None
-		Result:
-			None
-		"""
-		pass
